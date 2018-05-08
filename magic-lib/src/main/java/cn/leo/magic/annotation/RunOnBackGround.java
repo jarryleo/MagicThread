b840c6b9dc155e835bd79bdc13c958708cf4dfd7
liujiarui <liujiarui@ship56.net>
2018-05-08 18:21:13 +0800

    Initial commit

---
+++ magic-lib/src/main/java/cn/leo/magic/annotation/RunOnBackGround.java
@@ -0,0 +1,15 @@
+package cn.leo.magic.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Leo on 2018/5/2.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RunOnBackGround {
+
+}
---
+++ magic-lib/src/main/java/cn/leo/magic/aspect/ThreadAspect.java
@@ -0,0 +1,46 @@
+package cn.leo.magic.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+/**
+ * Created by Leo on 2018/5/2.
+ */
+@Aspect
+public class ThreadAspect {
+    private static final String UI_THREAD =
+            "execution(@cn.leo.magic.annotation.RunOnUIThread * *(..))";
+    private static final String IO_THREAD =
+            "execution(@cn.leo.magic.annotation.RunOnIOThread * *(..))";
+    private static final String BACK_THREAD =
+            "execution(@cn.leo.magic.annotation.RunOnBackGround * *(..))";
+
+    @Pointcut(UI_THREAD)
+    public void methodRunOnUIThread() {
+    }
+
+    @Pointcut(IO_THREAD)
+    public void methodRunOnIOThread() {
+    }
+
+    @Pointcut(BACK_THREAD)
+    public void methodRunOnBackGround() {
+    }
+
+    @Around("methodRunOnUIThread()")
+    public void aroundJoinPointUI(final ProceedingJoinPoint joinPoint) throws Throwable {
+        joinPoint.proceed();
+    }
+
+    @Around("methodRunOnIOThread()")
+    public void aroundJoinPointIO(final ProceedingJoinPoint joinPoint) throws Throwable {
+        joinPoint.proceed();
+    }
+
+    @Around("methodRunOnBackGround()")
+    public void aroundJoinPointBack(final ProceedingJoinPoint joinPoint) throws Throwable {
+        joinPoint.proceed();
+    }
+}
